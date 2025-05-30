fdb1f89557aa67c3599b954c6c81320ddc709a5d
Tr0llinMe <huydavidpham@hotmail.com>
2025-05-29 22:17:52 -0400

    Overhaul Website Part 3
    Cutting off code that is not needed for the site.

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/home/Home';
-import About from './components/about/About'; 
 import Projects from './components/projects/Projects';
-import Contact from './components/contact/Contact';
 import './App.css';
 
-// Dont need about, contact so far.
+// Dont need about so far.
 function AppRouter() {
   return (
     <Router>
@@ -14,8 +12,6 @@ function AppRouter() {
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} /> 
-            <Route path="/contact" element={<Contact />} />
             <Route path="/projects/:projectId" element={<Projects />} />
           </Routes>
         </div>
